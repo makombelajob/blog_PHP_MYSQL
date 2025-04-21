f5554bb58771f59875863d68f52d7664721e0878
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-04-21 19:57:21 +0200

    delete_category.php done

--- app/delete_category.php
+++ app/delete_category.php
@@ -3,23 +3,49 @@ global $pdo;
 require_once 'includes/dbconnect.php';
 $errorsMsg = $finalMsg = [];
 $id = $_GET['id'];
-if(is_numeric($id)){
-    try {
-        // commande sql
-        $sql = 'SELECT name, description FROM categories WHERE id = :category_id';
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':category_id', $id, PDO::PARAM_INT);
-        $exec = $stmt->execute();
-        if($exec){
-            $finalMsg['successBind'] = 'Category founded !';
-            $category = $stmt->fetch();
+if(!is_numeric($id)){
+    $errorsMsg['idPlaced'] = 'Checking failed !!!';
+}else{
+    $sql = 'SELECT id,name FROM categories WHERE id = :category_id;';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':category_id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $category = $stmt->fetch();
+
+    // verification of inputs
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $categoryName = htmlspecialchars(trim($_POST['name'])) ?? '';
+        if(empty($categoryName) || strlen($categoryName) > 50){
+            $errorsMsg['name'] = 'Le nom doit être valide !';
         }
+        if(!empty($errorsMsg)){
+            $finalMsg['failed'] = 'Veuillez suivre les instructions !';
+        }else{
+            // sql commande
+            try{
+                $sql = 'DELETE FROM posts_categories WHERE categories_id = :post_category_id;';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':post_category_id', $id, PDO::PARAM_INT);
+                $execPost = $stmt->execute();
 
-    }catch(PDOException $e){
-        $errorsMsg['id'] = $e->getMessage();
-        $finalMsg['failedBind'] = 'Category not found !';
+                $sql = 'DELETE FROM categories WHERE id = :category_id;';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':category_id', $id, PDO::PARAM_INT);
+                $exeCategory = $stmt->execute();
+
+                if(!$exeCategory || !$execPost){
+                    $finalMsg['failedDelete'] = 'La suppression a échouée ! 🤦‍♂️';
+                }else{
+                    $finalMsg['successDelete'] = 'La suppression a réussit ! 👍💾🌏';
+                    header('Refresh:3; url=index.php');
+                }
+            }catch (PDOException $e){
+                $finalMsg['failedDelete'] = $e->getMessage();
+
+            }
+
+        }
     }
-
 }
 
 require_once 'includes/header.php';
@@ -29,19 +55,22 @@ require_once 'includes/header.php';
         <form action="" method="post">
             <div class="name">
                 <label class="form-label" for="name">Nom</label>
-                <input class="form-control" type="text" name="name" placeholder="Veuillez Entrer le nom" value="<?= $category['name'] ;?>"/>
+                <input id="<?= $category['id'] ;?>" class="form-control" type="text" name="name" placeholder="Veuillez Entrer le nom"  value="<?= $category['name'] ?? '' ;?>"/>
+                <?php if(isset($errorsMsg) ?? '') : ;?>
+                    <p class="text-center text-danger"><?= $errorsMsg['name'] ?? '';?></p>
+                <?php endif;?>
             </div>
-            <div class="description">
+            <div class="my-2">
                 <label class="form-label" for="description">Description</label>
-                <textarea class="form-control" name="description" id="description" cols="30" rows="10" value="<?= $category['description'] ;?>"></textarea>
+                <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
             </div>
             <div class="text-center m-4">
                 <button class="btn btn-danger" type="submit">Suppression</button>
             </div>
         </form>
         <div class="text-center bg-warning rounded-3">
-            <?php if(isset($errorsMsg) || isset($finalMsg)) : ;?>
-                <p><?= $errorsMsg['id'] ?? $finalMsg['successBind'] ?? $finalMsg['failedBind'] ?? '' ;?></p>
+            <?php if(isset($errorsMsg) ?? isset($finalMsg) ?? '') : ;?>
+                <p><?= $errorsMsg['idPlaced'] ?? $finalMsg['failed'] ?? $finalMsg['failedDelete'] ?? $finalMsg['successDelete'] ?? '' ;?></p>
             <?php endif;?>
         </div>
     </main>
